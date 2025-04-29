295efc859e89c47e767499f92202e28a2ce2044c
xjh22222228 <xjh22222228@gmail.com>
2025-04-29 15:14:59 +0800

    refactor: remove unnecessary semicolons and standardize formatting in configuration and initialization scripts

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -10,17 +10,17 @@
 // 回滚最近的迁移:
 // npm run migration:revert
 
-import { DataSource } from 'typeorm';
-import * as dotenv from 'dotenv';
-import * as path from 'path';
-import * as tsconfig from 'tsconfig-paths';
+import { DataSource } from 'typeorm'
+import * as dotenv from 'dotenv'
+import * as path from 'path'
+import * as tsconfig from 'tsconfig-paths'
 
 // 注册路径别名
-const { absoluteBaseUrl, paths } = require('./tsconfig.json').compilerOptions;
+const { absoluteBaseUrl, paths } = require('./tsconfig.json').compilerOptions
 tsconfig.register({
   baseUrl: absoluteBaseUrl || '.',
   paths: paths || { '@/*': ['src/*'] },
-});
+})
 
 // 导入转换器工具
 // import {
@@ -31,11 +31,11 @@ tsconfig.register({
 // global.numberTransformer = numberTransformer;
 
 // 根据环境加载不同的 .env 文件
-const env = process.env.NODE_ENV || 'local';
-const envFilePath = `.env.${env}`;
-dotenv.config({ path: path.resolve(process.cwd(), envFilePath) });
+const env = process.env.NODE_ENV || 'local'
+const envFilePath = `.env.${env}`
+dotenv.config({ path: path.resolve(process.cwd(), envFilePath) })
 
-console.log('ENV:', path.resolve(process.cwd(), envFilePath), process.env);
+console.log('ENV:', path.resolve(process.cwd(), envFilePath), process.env)
 
 export default new DataSource({
   type: 'mysql',
@@ -47,4 +47,4 @@ export default new DataSource({
   entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   timezone: '+08:00',
-});
+})
