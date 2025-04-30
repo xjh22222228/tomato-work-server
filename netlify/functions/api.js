78fe8964f3611e3f410ea1c8707da50b6c1704dd
xjh22222228 <xjh22222228@gmail.com>
2025-04-30 15:11:37 +0800

    feat: Add Netlify configuration and update API module path for serverless deployment

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from '../../src/app.module'
+import { AppModule } from '../../dist/src/app.module.js'
 import serverless from 'serverless-http'
 import { ValidationPipe } from '@nestjs/common'
 
